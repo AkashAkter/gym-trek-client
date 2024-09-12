7b14dd42e730a9d92b21b7feb1e88609a7bad0d2
AkashAkter <akash31akter@gmail.com>
2024-09-12 15:23:31 +0600

    PROJECT CREATED FOR FRONTEND

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <p className="text-orange-900 font-extrabold text-xl flex justify-center h-screen items-center">
+        PROJECT CREATED
+      </p>
+    </>
+  );
+}
+
+export default App;
